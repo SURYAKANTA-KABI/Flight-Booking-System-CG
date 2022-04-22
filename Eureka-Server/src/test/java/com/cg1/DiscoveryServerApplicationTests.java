f073b6f94a1b4b8f4cab2aabd6958bb8ec61d23f
Suryakanta <75796406+SURYAKANTA-KABI@users.noreply.github.com>
2022-04-22 09:41:40 +0530

    Add files via upload

---
+++ Eureka-Server/src/test/java/com/cg1/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
